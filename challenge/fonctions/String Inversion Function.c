d226c4bdce3e63daa5206879c37c02d89f39c60a
9asdaoui <oussamaqasdaoui@gmail.com>
2024-09-29 20:10:27 +0200

    sas

--- challenge/fonctions/String Inversion Function.c
+++ challenge/fonctions/String Inversion Function.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
 
 void fac(int size,int tab[size]){
-    for (int u=0;u<size;u++){
-            printf("%d ",tab[u]);
-    }
-}
-int main(){
-
-
-    int nume,numbers;
 
 
 
-    printf("enter the number of the elements : ");
-    scanf("%d",&nume);
-    int A[nume];
-    int b;
-    int i,u;
-    for(i=nume-1;i>=0 ;i--){
-            printf(" enter number : ");
-            scanf("%d",&A[i]);
-    }
-    fac(nume,A[nume]);
 return 0;
 }
 
